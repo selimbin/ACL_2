ce905837d68631d55e2c4f7eb0717a67724e8210
selimbin <selim.elindary@student.guc.edu.eg>
2020-12-17 15:28:47 +0200

    fourth commit

--- models/staff.js
+++ models/staff.js
@@ -1,64 +1,26 @@
 const mongoose = require('mongoose')
 
-const courseSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        minlength:3,
-        required:true
-    },
-    code:{
-        type:String,
-        minlength:3,
-        required:true,
-        unique:true
-    },
-    teachingSlot:{
-        type:Number
-    },
-    department:departmentSchema,
-    lecturer:[staffSchema],
-    TA:[staffSchema]
-})
-const facultySchema = new mongoose.Schema({
-    name:{
-        type:String,
-        minlength:3,
-        required:true,
-        unique:true
-    },
-    departments:[departmentSchema],
-    dean:staffSchema
-})
-const departmentSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        minlength:3,
-        required:true,
-        unique:true
-    },
-    head:staffSchema,
-    TA:[staffSchema]
-})
-const locationSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        minlength:2,
-        required:true,
-        unique:true
-    },
-    building:{
-        type:String,
-        required:true
-    },
-    type:{
-        type:String,
-        required:true
-    },
-    capacity:{
-        type:Number,
-        required:true
-    }
-})
+
+// const locationSchema = new mongoose.Schema({
+//     name:{
+//         type:String,
+//         minlength:2,
+//         required:true,
+//         unique:true
+//     },
+//     building:{
+//         type:String,
+//         required:true
+//     },
+//     type:{
+//         type:String,
+//         required:true
+//     },
+//     capacity:{
+//         type:Number,
+//         required:true
+//     }
+// })
 const staffSchema = new mongoose.Schema({
     name:{
         type:String,
@@ -96,16 +58,17 @@ const staffSchema = new mongoose.Schema({
     },
     accumelatedHours:{
         type:Number
-    },
-    department:[departmentSchema],
-    course:[courseSchema]
+    }
+    // department:[departmentSchema],
+    // course:[courseSchema]
 },
 {
     strict:false,
     timestamps:true
 })
 module.exports = mongoose.model('Staff', staffSchema)
-module.exports= mongoose.model('Department', departmentSchema)
-module.exports = mongoose.model('Faculty', facultySchema)
-module.exports= mongoose.model('Course', courseSchema)
-module.exports= mongoose.model('Location', locationSchema)
+module.exports.staffSchema=staffSchema
+// module.exports= mongoose.model('Department', departmentSchema)
+// module.exports = mongoose.model('Faculty', facultySchema)
+// module.exports= mongoose.model('Course', courseSchema)
+// module.exports= mongoose.model('Location', locationSchema)
